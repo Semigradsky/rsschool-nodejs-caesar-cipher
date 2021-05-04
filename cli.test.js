6aeac220a024142de6024e181cd9b77f598397b9
Dmitry Semigradsky <semigradskyd@gmail.com>
2021-05-04 14:46:38 +0300

    test: add code coverage, improved test

--- cli.test.js
+++ cli.test.js
@@ -24,14 +24,14 @@ test('Error should be shown if required flag (--shift) is missing', async t => {
 })
 
 test('Error should be shown if --action flag is invalid', async t => {
-  const error = await t.throwsAsync(() => execa('./cli.js', ['--action', 'foo']))
+  const error = await t.throwsAsync(() => execa('./cli.js', ['--action', 'foo', '--shift', '10']))
 
   t.is(error.exitCode, 2)
   t.assert(error.stderr.length > 0)
 })
 
 test('Error should be shown if --shift flag is invalid', async t => {
-  const error = await t.throwsAsync(() => execa('./cli.js', ['--shift', '2.3']))
+  const error = await t.throwsAsync(() => execa('./cli.js', ['--action', 'encode', '--shift', '2.3']))
 
   t.is(error.exitCode, 2)
   t.assert(error.stderr.length > 0)
