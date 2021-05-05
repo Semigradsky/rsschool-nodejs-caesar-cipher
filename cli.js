abeea107f4d4f85474c0a1e0ab72b35e5a25a2d7
Dmitry Semigradsky <semigradskyd@gmail.com>
2021-05-05 19:48:27 +0300

    refactor: move input to separate file, move IO error handling to 'streams'

--- cli.js
+++ cli.js
@@ -1,54 +1,11 @@
 #!/usr/bin/env node
-import meow from 'meow'
-import { createDecipher, createCipher } from './cipher-stream.js'
+import { getInput } from './input.js'
+import { createCipher, createDecipher } from './cipher-stream.js'
 import { createReadStream, createWriteStream } from './streams.js'
 
-const cli = meow(`
-  Usage
-    $ caesar-cipher-cli --action encode --shift <number> [--input "./input.txt"] [--output "./output.txt"]
-    $ caesar-cipher-cli --action decode --shift <number> [--input "./input.txt"] [--output "./output.txt"]
-`, {
-  flags: {
-    action: {
-      alias: 'a',
-      type: 'string',
-      isRequired: true,
-    },
-    shift: {
-      alias: 's',
-      type: 'number',
-      isRequired: true,
-    },
-    input: {
-      alias: 'i',
-      type: 'string',
-    },
-    output: {
-      alias: 'o',
-      type: 'string',
-    },
-  },
-})
-
-const {action, shift, input, output} = cli.flags
-
-if (action !== 'encode' && action !== 'decode') {
-  console.error(`Invalid --action flag: '${action}'. Valid actions: 'encode' and 'decode'.`)
-  process.exit(2)
-}
-
-if (!Number.isFinite(shift) || Math.ceil(shift) !== shift) {
-  console.error('Invalid --shift flag. Shift must be integer.')
-  process.exit(2)
-}
+const { action, shift, input, output } = getInput()
 
 const readable = createReadStream(input)
-  .on('error', () => {
-    console.error(`Failed! Can't open '${input}'.`)
-    console.error(`File doesn't exist or not accessible, please ensure that path is correct.`)
-    process.exit(2)
-  })
-
 const writable = createWriteStream(output)
 
 readable
@@ -57,8 +14,3 @@ readable
     : createDecipher(shift)
   )
   .pipe(writable)
-  .on('error', () => {
-    console.error(`Failed! Can't write to '${output}'.`)
-    console.error(`File doesn't exist or not accessible, please ensure that path is correct.`)
-    process.exit(2)
-  })
--- streams.js
+++ streams.js
@@ -1,19 +1,30 @@
 import fs from 'fs'
 
 export const createReadStream = (input) => {
-  return input
-    ? fs.createReadStream(input, {
-      encoding: 'utf8',
-      flags: 'r',
+  if (!input) {
+    return process.stdin
+  }
+
+  return fs.createReadStream(input, { encoding: 'utf8' })
+    .on('error', () => {
+      console.error(`Failed! Can't open '${input}'.`)
+      console.error(`File doesn't exist or not accessible, please ensure that path is correct.`)
+      process.exit(2)
     })
-    : process.stdin
 }
 
 export const createWriteStream = (output) => {
-  return output
-    ? fs.createWriteStream(output, {
-      encoding: 'utf8',
-      flags: 'a',
+  if (!output) {
+    return process.stdout
+  }
+
+  return fs.createWriteStream(output, {
+    encoding: 'utf8',
+    flags: 'a',
+  })
+    .on('error', () => {
+      console.error(`Failed! Can't write to '${output}'.`)
+      console.error(`File doesn't exist or not accessible, please ensure that path is correct.`)
+      process.exit(2)
     })
-    : process.stdout
 }
